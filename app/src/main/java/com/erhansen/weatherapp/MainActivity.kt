d431105f65b531b814aabaee71820e1ece8a1792
erhansennx <erhanfatih33@gmail.com>
2022-08-15 12:44:04 +0300

    Commit mail changed. This is a test.

--- app/src/main/java/com/erhansen/weatherapp/MainActivity.kt
+++ app/src/main/java/com/erhansen/weatherapp/MainActivity.kt
@@ -10,9 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("hello weather app")
-
-
     }
 
 
